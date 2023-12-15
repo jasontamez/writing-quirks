fcba203c10e420b27891cae26e078d0f36833899
Jason Tamez <jasontamez@users.noreply.github.com>
2023-12-14 20:46:25 -0600

    adjusting spaces/tabs/other things *setting up color theme *planning where to go in the future

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact
+	IonApp,
+	IonIcon,
+	IonLabel,
+	IonRouterOutlet,
+	IonTabBar,
+	IonTabButton,
+	IonTabs,
+	setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
@@ -38,40 +38,52 @@ import './theme/variables.css';
 setupIonicReact();
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
+	<IonApp>
+		<IonReactRouter>
+			<IonTabs>
+				<IonRouterOutlet>
+					<Route exact path="/tab1">
+						<Tab1 />
+					</Route>
+					<Route exact path="/tab2">
+						<Tab2 />
+					</Route>
+					<Route path="/tab3">
+						<Tab3 />
+					</Route>
+					<Route exact path="/">
+						<Redirect to="/tab1" />
+					</Route>
+				</IonRouterOutlet>
+				<IonTabBar slot="bottom">
+				<IonTabButton tab="tab1" href="/tab1">
+						<IonIcon aria-hidden="true" icon={triangle} />
+						<IonLabel>Story Prompts</IonLabel>
+					</IonTabButton>
+					<IonTabButton tab="tab2" href="/tab2">
+						<IonIcon aria-hidden="true" icon={ellipse} />
+						<IonLabel>Taverns and Inns</IonLabel>
+					</IonTabButton>
+					<IonTabButton tab="tab3" href="/tab3">
+						<IonIcon aria-hidden="true" icon={square} />
+						<IonLabel>Suburban Street Names</IonLabel>
+					</IonTabButton>
+					<IonTabButton tab="tab1" href="/tab1">
+						<IonIcon aria-hidden="true" icon={triangle} />
+						<IonLabel>Technobabble</IonLabel>
+					</IonTabButton>
+					<IonTabButton tab="tab2" href="/tab2">
+						<IonIcon aria-hidden="true" icon={ellipse} />
+						<IonLabel>Shakespearian Insults</IonLabel>
+					</IonTabButton>
+					<IonTabButton tab="tab3" href="/tab3">
+						<IonIcon aria-hidden="true" icon={square} />
+						<IonLabel>Really Odd Flavors</IonLabel>
+					</IonTabButton>
+				</IonTabBar>
+			</IonTabs>
+		</IonReactRouter>
+	</IonApp>
 );
 
 export default App;
